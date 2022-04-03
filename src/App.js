dfbc8193797523bc475b11cfde0286a5dabbae17
Noor-Hashem77 <noor.hashem@applyboard.com>
2022-04-03 00:21:59 -0400

    rahmi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Rahma!
+          Hi Rahmi!!
         </p>
         <a
           className="App-link"
